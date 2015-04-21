b2fe9b357ad1f77a86491669bff2f351fc26c260
ohrl <sdzg@macmini.local>
2015-04-21 16:41:31 +0800

    Initial Commit

---
+++ BluetoothControl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BluetoothControl
+//
+//  Created by sdzg on 15/4/21.
+//  Copyright (c) 2015年 646767424@qq.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BluetoothControl/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BluetoothControl
+//
+//  Created by sdzg on 15/4/21.
+//  Copyright (c) 2015年 646767424@qq.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
